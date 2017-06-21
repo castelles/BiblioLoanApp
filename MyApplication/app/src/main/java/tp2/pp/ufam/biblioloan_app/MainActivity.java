4b7e04e272e5bd3ffe7d8f8283fb7819186a85c4
castelles <cast@icomp.ufam.edu.br>
2017-06-21 19:49:38 -0400

    Projeto criado

---
+++ MyApplication/app/src/main/java/tp2/pp/ufam/biblioloan_app/MainActivity.java
@@ -0,0 +1,13 @@
+package tp2.pp.ufam.biblioloan_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
