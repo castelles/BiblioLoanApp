9807ff2d48f5e456df4fa61c925e1986562c8b3d
castelles <cast@icomp.ufam.edu.br>
2017-06-26 21:41:30 -0400

    Final 26.06

--- MyApplication/app/src/main/java/tp2/pp/ufam/biblioloan_app/Database.java
+++ MyApplication/app/src/main/java/tp2/pp/ufam/biblioloan_app/Database.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteDatabase;
 
 public class Database extends SQLiteOpenHelper
 {
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "LibraryDB.db";
     public static final String SQL_CREATE_TABLE_USER = "CREATE TABLE Usuarios(" +
             "login TEXT PRIMARY KEY, password TEXT, name TEXT, canloan INT)";
--- MyApplication/app/src/main/java/tp2/pp/ufam/biblioloan_app/TitlesListActivity.java
+++ MyApplication/app/src/main/java/tp2/pp/ufam/biblioloan_app/TitlesListActivity.java
@@ -1,14 +1,23 @@
 package tp2.pp.ufam.biblioloan_app;
 
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.ListActivity;
 
 public class TitlesListActivity extends ListActivity {
 
+    private TitulosDAO titlesDAO;
+    private SimpleCursorAdapter data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_titles_list);
+
+        this.titlesDAO = new TitulosDAO(this);
+
+        data = new SimpleCursorAdapter(this, R.layout.title_row, titlesDAO.getTitles(), new String[] {"title", "author", "edition"},
+                new int[] { R.id.titleText, R.id.authorText, R.id.editionText}, 0);
+        setListAdapter(data);
     }
 }
